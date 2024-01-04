7303ee18d778fcab4824ae5266119dde32303e2e
danica <danica.for.github@gmail.com>
2024-01-04 16:17:41 +0800

    update

---
+++ 20240104-asyncImportOneDependenceButWepackGiveTwoChunk/emptyPackLikeThis.js
@@ -0,0 +1,35 @@
+(this["webpackJsonp-app-name"] = this["webpackJsonp-app-name"] || []).push([[1],[
+  /* 0 */,
+  /* 1 */,
+  /* 2 */
+  /*!********************!*\
+    !*** fs (ignored) ***!
+    \********************/
+  /*! no static exports found */
+  /***/ (function(module, exports) {
+  
+  /* (ignored) */
+  
+  /***/ }),
+  /* 3 */
+  /*!************************!*\
+    !*** crypto (ignored) ***!
+    \************************/
+  /*! no static exports found */
+  /***/ (function(module, exports) {
+  
+  /* (ignored) */
+  
+  /***/ }),
+  /* 4 */
+  /*!************************!*\
+    !*** stream (ignored) ***!
+    \************************/
+  /*! no static exports found */
+  /***/ (function(module, exports) {
+  
+  /* (ignored) */
+  
+  /***/ })
+  ]]);
+  //# sourceMappingURL=1.chunk.js.map
